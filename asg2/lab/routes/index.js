7d97b329403a3906ddad493c6398d552a251b78a
Taylor Dulleakellegrew <twdulleakellegrew@email.neit.edu>
2017-10-15 13:47:23 -0400

    Lab 2 form completed

---
+++ asg2/lab/routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/about', function(req, res, next){
+  res.render('about', {title: 'About'});
+});
+
+router.get('/form', function(req, res, next){
+  res.render('form', {title: 'Form'});
+});
+
+module.exports = router;
